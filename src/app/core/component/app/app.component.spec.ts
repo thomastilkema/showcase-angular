9ad4b1725881ea535a6e12a96c61d58381a1204a
Thomas Tilkema <thomastilkema@users.noreply.github.com>
2023-01-31 20:46:57 +0100

    fix: keep the user logged in on reload

--- src/app/core/component/app/app.component.spec.ts
+++ src/app/core/component/app/app.component.spec.ts
@@ -1,5 +1,9 @@
 import { RouterTestingModule } from '@angular/router/testing';
+import { UserService } from '@app/core/service';
+import { UserAction } from '@app/store/user';
+import { getMockedUser } from '@app/test';
 import { createComponentFactory } from '@ngneat/spectator';
+import { Store } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { ComponentPageObject } from './app.component.page-object';
 
@@ -7,12 +11,47 @@ describe('The <tms-app> component', () => {
   const createSpectator = createComponentFactory({
     component: AppComponent,
     imports: [RouterTestingModule],
+    mocks: [Store, UserService],
   });
 
-  const setup = () => new ComponentPageObject(createSpectator());
+  const setup = (hasUserInStorage = false) => {
+    const spectator = createSpectator({ detectChanges: false });
+    const pageObject = new ComponentPageObject(spectator);
+
+    const store = spectator.inject(Store);
+    const userService = spectator.inject(UserService);
+    userService.getUserFromStorage.and.returnValue(
+      hasUserInStorage ? getMockedUser() : undefined
+    );
+
+    pageObject.detectChanges();
+
+    return {
+      pageObject,
+      store,
+      userService,
+    };
+  };
 
   it('should display the routed content', () => {
-    const pageObject = setup();
+    const { pageObject } = setup();
     expect(pageObject.getRoutedContent()).toBeTruthy();
+  });
+
+  it('should check if there is a user saved in storage', () => {
+    const { userService } = setup();
+    expect(userService.getUserFromStorage).toHaveBeenCalledOnceWith();
+  });
+
+  it('should not update the state when there is no user saved in storage', () => {
+    const { store } = setup();
+    expect(store.dispatch).not.toHaveBeenCalled();
+  });
+
+  it('should update the state with the user saved in storage as the logged in user', () => {
+    const { store } = setup(true);
+    expect(store.dispatch).toHaveBeenCalledOnceWith(
+      UserAction.login(getMockedUser())
+    );
   });
 });
--- src/app/core/component/app/app.component.ts
+++ src/app/core/component/app/app.component.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserAction } from '@app/store/user';
+import { Store } from '@ngrx/store';
+import { UserService } from '../../service';
 
 @Component({
   selector: 'tms-app',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(
+    private readonly store: Store,
+    private readonly userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    // Get user from storage since we don't use a real api
+    const userFromStorage = this.userService.getUserFromStorage();
+    if (userFromStorage) {
+      this.store.dispatch(UserAction.login(userFromStorage));
+    }
+  }
+}
